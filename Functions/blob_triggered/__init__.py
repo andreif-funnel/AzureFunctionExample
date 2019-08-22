73747b16e9363d8b51721f221bf2d2c6500c342f
Andrei Fokau <andrei@funnel.io>
2019-08-22 16:31:07 +0200

    Move blobs to s3.

--- Functions/blob_triggered/__init__.py
+++ Functions/blob_triggered/__init__.py
@@ -1,12 +1,32 @@
 import logging
+import os
 
-import azure.functions as func
+import azure.functions
+import azure.storage.blob
+import boto3
 
 
-def main(blin: func.InputStream):
+def main(blin: azure.functions.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {blin.name}\n"
                  f"Blob Size: {blin.length} bytes")
 
     data = blin.read()
     logging.info(data)
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
+    filename = os.path.basename(blin.name)
+    token, filename = filename.split('_')
+    bucket = os.environ["AWS_S3_BUCKET"]
+    s3_key = f"{token}/{filename}"
+    s3.put_object(Bucket=bucket, Key=s3_key, Body=blin.read())
+    s3.get_waiter("object_exists").wait(
+        Bucket=bucket, Key=s3_key, WaiterConfig={"Delay": 2, "MaxAttempts": 5},
+    )
+    bs = azure.storage.blob.BlockBlobService(connection_string=os.environ["AzureWebJobsStorage"])
+    bs.delete_blob(container_name="my-container", blob_name=blin.name)
+    logging.info([b.name for b in bs.list_blobs(container_name="my-container")])
