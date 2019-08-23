1a4612dbe079064b3bbeeea7498261bb886d8670
Andrei Fokau <andrei@funnel.io>
2019-08-23 15:37:28 +0200

    Fix deleting file.

--- Functions/blob_triggered/__init__.py
+++ Functions/blob_triggered/__init__.py
@@ -29,7 +29,6 @@ def main(blin: azure.functions.InputStream):
     )
     bs = azure.storage.blob.BlockBlobService(connection_string=os.environ["AzureWebJobsStorage"])
     container = "my-container"
-    logging.info([b.name for b in bs.list_blobs(container_name=container)])
+    filename = blin.name.partition("/")[-1]
     logging.info(f"Deleting {filename}...")
     bs.delete_blob(container_name=container, blob_name=filename)
-    logging.info([b.name for b in bs.list_blobs(container_name=container)])
